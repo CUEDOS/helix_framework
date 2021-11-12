23be636f1edb9596980b8fff44218ff1cdee06dd
Verdon Crann <verdon.crann@manchester.ac.uk>
2021-11-12 09:16:45 +0000

    reformatted

--- testing/range_test_airside_sitl.py
+++ testing/range_test_airside_sitl.py
@@ -1,18 +1,21 @@
 import time
 import asyncio
 from mavsdk import System
-from mavsdk.offboard import (OffboardError, VelocityNedYaw)
+from mavsdk.offboard import OffboardError, VelocityNedYaw
 import paho.mqtt.client as mqtt
+
+
 class drone_telemetry:
-    qx = 0          # position of the quadrotor in x direction with respect to initial point
-    qy = 0          # position of the quadrotor in y direction with respect to initial point
-    qz = 0          # position of the quadrotor in z direction with respect to initial point
+    qx = 0  # position of the quadrotor in x direction with respect to initial point
+    qy = 0  # position of the quadrotor in y direction with respect to initial point
+    qz = 0  # position of the quadrotor in z direction with respect to initial point
+
 
 async def run():
     mqtt_topic = "range_test"
     broker_IP = "localhost"
     client = mqtt.Client()
-    client.connect(broker_IP,1883,60)
+    client.connect(broker_IP, 1883, 60)
     # Init the drone
     drone = System()
     await drone.connect()
@@ -21,26 +24,39 @@ async def run():
         if state.is_connected:
             print(f"Drone discovered!")
             break
-    
-    #Start background task
+
+    # Start background task
     asyncio.ensure_future(get_position(drone))
-    # End of Init the drone   
-    # Endless loop 
+    # End of Init the drone
+    # Endless loop
     while 1:
-        start_time=time.time()
-        client.publish(mqtt_topic, str(time.time())+","+str(drone_telemetry.qx)+","+str(drone_telemetry.qy)+","+str(drone_telemetry.qz));
-        
-        #Checking frequency of the loop
-        await asyncio.sleep(0.1-(time.time()-start_time)) # to make while 1 work at 10 Hz
-        print("loop duration=",(time.time()-start_time))
+        start_time = time.time()
+        client.publish(
+            mqtt_topic,
+            str(time.time())
+            + ","
+            + str(drone_telemetry.qx)
+            + ","
+            + str(drone_telemetry.qy)
+            + ","
+            + str(drone_telemetry.qz),
+        )
+
+        # Checking frequency of the loop
+        await asyncio.sleep(
+            0.1 - (time.time() - start_time)
+        )  # to make while 1 work at 10 Hz
+        print("loop duration=", (time.time() - start_time))
     # End of Endless loop
 
-#runs in background and upates state class with latest telemetry
+
+# runs in background and upates state class with latest telemetry
 async def get_position(drone):
     async for position_velocity_ned in drone.telemetry.position_velocity_ned():
-            drone_telemetry.qx = position_velocity_ned.position.north_m
-            drone_telemetry.qy = position_velocity_ned.position.east_m
-            drone_telemetry.qz = position_velocity_ned.position.down_m
+        drone_telemetry.qx = position_velocity_ned.position.north_m
+        drone_telemetry.qy = position_velocity_ned.position.east_m
+        drone_telemetry.qz = position_velocity_ned.position.down_m
+
 
 if __name__ == "__main__":
     # Start the main function
