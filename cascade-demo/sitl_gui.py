a3d866b764aad64cc2bb01e8e6753da255ac0ba9
Verdon Crann <verdon.crann@manchester.ac.uk>
2021-11-22 15:30:08 +0000

    refactored sitl_gui to remove global variables

--- cascade-demo/sitl_gui.py
+++ cascade-demo/sitl_gui.py
@@ -6,225 +6,214 @@ import mavsdk
 import os
 import signal
 import tkinter as tk
-from tkinter import ttk
-from tkinter import filedialog
-from tkinter import messagebox
-from tkinter import *
 import paho.mqtt.client as mqtt
 
-# Global Variables
-gazebo = None
 
-# this is the function called when the button is clicked
-def StartClickFunction():
-    send_command("start")
+class App:
+    def __init__(self, master):
+        self.master = master
+        self.check_var = tk.IntVar(value=1)
+        self.gazebo_process = None
+        self.mavsdk_process = [None]
+        self.script_process = [None]
 
+        # This is the section of code which creates the main window
+        master.rowconfigure(tuple(range(2)), minsize=100)
+        master.grid_columnconfigure(tuple(range(2)), uniform="button", minsize=220)
 
-# this is the function called when the button is clicked
-def StopClickFunction():
-    send_command("stop")
+        self.sitl_frame = tk.Frame(master)
 
+        self.sitl_frame.columnconfigure(tuple(range(3)), weight=1)
+        self.sitl_frame.grid(row=3, column=0, columnspan=2, sticky="ew")
 
-def TakeOffClickFunction():
-    send_command("takeoff")
+        self.launch_btn = tk.Button(
+            self.sitl_frame,
+            text="Launch SITL Simulation",
+            bg="#008B45",
+            font=("arial", 12, "normal"),
+            command=self.LaunchClickFunction,
+        )
+        self.launch_btn.grid(row=5, column=0, columnspan=3, sticky="e")
 
+        self.select_firmware_btn = tk.Button(
+            self.sitl_frame,
+            text="Select",
+            # bg="#008B45",
+            font=("arial", 12, "normal"),
+            command=self.SelectClickFunction,
+        )
+        self.select_firmware_btn.grid(row=4, column=2, sticky="e")
 
-def LandClickFunction():
-    send_command("land")
+        tk.Button(
+            master,
+            text="Start",
+            bg="#008B45",
+            font=("arial", 12, "normal"),
+            command=self.StartClickFunction,
+        ).grid(
+            row=1,
+            column=0,
+            sticky="nesw",
+        )
 
+        tk.Button(
+            master,
+            text="Hold",
+            bg="#CD4F39",
+            font=("arial", 12, "normal"),
+            command=self.StopClickFunction,
+        ).grid(row=0, column=1, sticky="nesw")
 
-def LaunchClickFunction():
-    print("launch")
-    if validate_int(drone_no_entry.get()) is True:
-        no_drones = int(drone_no_entry.get())
-    else:
-        messagebox.showinfo("Error", "Please enter an Int")
-    global gazebo
-    gazebo = subprocess.Popen(
-        ["bash", "gazebo_sitl_multiple_run.sh", "-n", str(no_drones)],
-        cwd=path_label.cget("text") + "/Tools/",
-        stdin=None,
-        stdout=None,
-        stderr=None,
-        preexec_fn=os.setsid,
-    )  # last argument important to allow process to be killed
+        tk.Button(
+            master,
+            text="Take Off",
+            bg="#CD4F39",
+            font=("arial", 12, "normal"),
+            command=self.TakeOffClickFunction,
+        ).grid(row=0, column=0, sticky="nesw")
 
-    global mavsdk_server
-    mavsdk_server = [None] * no_drones
-    global script
-    script = [None] * no_drones
+        tk.Button(
+            master,
+            text="Land",
+            bg="#CD4F39",
+            font=("arial", 12, "normal"),
+            command=self.LandClickFunction,
+        ).grid(row=1, column=1, sticky="nesw")
 
-    for i in range(0, no_drones):
-        mavsdk_server[i] = subprocess.Popen(
-            [
-                "./mavsdk_server",
-                "gazebo_sitl_multiple_run.sh",
-                "-p",
-                str(50041 + i),
-                "udp://:" + str(14540 + i),
-            ],
-            cwd=os.path.split(mavsdk.__file__)[0] + "/bin",
+        self.sitl_check = tk.Checkbutton(
+            master,
+            text="SITL",
+            variable=self.check_var,
+            onvalue=1,
+            offvalue=0,
+            command=self.SITLCheckFunction,
+        )
+        self.sitl_check.grid(row=2, column=0, columnspan=2, sticky="w")
+
+        self.no_drones_label = tk.Label(self.sitl_frame, text="Number of Drones:")
+        self.no_drones_label.grid(row=3, column=0, sticky="w")
+
+        self.firmware_label = tk.Label(self.sitl_frame, text="PX4 Firmware Path:")
+        self.firmware_label.grid(row=4, column=0, sticky="w")
+
+        self.path_label = tk.Label(self.sitl_frame)
+        with open("config.txt") as f:
+            self.firmware_path = f.read()
+        self.path_label.config(text=self.firmware_path)
+        self.path_label.grid(row=4, column=1, sticky="w")
+
+        self.drone_no_entry = tk.Entry(self.sitl_frame)
+        self.drone_no_entry.insert(tk.END, "3")
+        self.drone_no_entry.grid(row=3, column=1, sticky="w")
+
+        master.title("Cascade Demo")
+        master.iconphoto(True, tk.PhotoImage(file="../img/cascade-logo.png"))
+        master.protocol("WM_DELETE_WINDOW", self.on_closing)
+        master.resizable(False, False)
+
+    # this is the function called when the button is clicked
+    def StartClickFunction(self):
+        self.send_command("start")
+
+    # this is the function called when the button is clicked
+    def StopClickFunction(self):
+        self.send_command("stop")
+
+    def TakeOffClickFunction(self):
+        self.send_command("takeoff")
+
+    def LandClickFunction(self):
+        self.send_command("land")
+
+    def LaunchClickFunction(self):
+        print("launch")
+        if self.validate_int(self.drone_no_entry.get()) is True:
+            self.no_drones = int(self.drone_no_entry.get())
+        else:
+            tk.messagebox.showinfo("Error", "Please enter an Int")
+        self.gazebo_process = subprocess.Popen(
+            ["bash", "gazebo_sitl_multiple_run.sh", "-n", str(self.no_drones)],
+            cwd=self.path_label.cget("text") + "/Tools/",
             stdin=None,
             stdout=None,
             stderr=None,
             preexec_fn=os.setsid,
-        )
+        )  # last argument important to allow process to be killed
 
-        script[i] = subprocess.Popen(
-            [
-                sys.executable,
-                "simple_flocking.py",
-                str(101 + i),
-                str(no_drones),
-                str(50041 + i),
-            ],
-            cwd=os.getcwd(),
-            preexec_fn=os.setsid,
-        )
+        self.mavsdk_process = [None] * self.no_drones
+        self.script_processs = [None] * self.no_drones
 
+        for i in range(0, self.no_drones):
+            self.mavsdk_process[i] = subprocess.Popen(
+                [
+                    "./mavsdk_server",
+                    "-p",
+                    str(50041 + i),
+                    "udp://:" + str(14540 + i),
+                ],
+                cwd=os.path.split(mavsdk.__file__)[0] + "/bin",
+                stdin=None,
+                stdout=None,
+                stderr=None,
+                preexec_fn=os.setsid,
+            )
 
-def SelectClickFunction():
-    print("select")
-    firmware_path = filedialog.askdirectory()
-    path_label.config(text=firmware_path)
-    with open("config.txt", "w") as f:
-        f.write(firmware_path)
+            self.script_process[i] = subprocess.Popen(
+                [
+                    sys.executable,
+                    "simple_flocking.py",
+                    str(101 + i),
+                    str(self.no_drones),
+                    str(50041 + i),
+                ],
+                cwd=os.getcwd(),
+                preexec_fn=os.setsid,
+            )
 
+    def SelectClickFunction(self):
+        print("select")
+        firmware_path = tk.filedialog.askdirectory()
+        self.path_label.config(text=firmware_path)
+        with open("config.txt", "w") as f:
+            f.write(firmware_path)
 
-def SITLCheckFunction():
-    print("function activated")
-    print(check_var.get())
-    if check_var.get() == 0:
-        sitl_frame.grid_remove()
-    else:
-        print("returning items")
-        sitl_frame.grid()
+    def SITLCheckFunction(self):
+        print("function activated")
+        print(self.check_var.get())
+        if self.check_var.get() == 0:
+            self.sitl_frame.grid_remove()
+        else:
+            print("returning items")
+            self.sitl_frame.grid()
 
+    # sends mqtt commands to broker
+    def send_command(self, command):
+        self.client = mqtt.Client()
+        self.client.connect("localhost", 1883, 60)
+        self.client.publish("commands", command)
+        self.client.disconnect()
 
-# sends mqtt commands to broker
-def send_command(command):
-    client = mqtt.Client()
-    client.connect("localhost", 1883, 60)
-    client.publish("commands", command)
-    client.disconnect()
-
-
-def validate_int(input):
-    if input in "0123456789":
-        try:
-            int(input)
-            return True
-        except ValueError:
+    def validate_int(self, input):
+        if input in "0123456789":
+            try:
+                int(input)
+                return True
+            except ValueError:
+                return False
+        else:
             return False
-    else:
-        return False
+
+    def on_closing(self):
+        if self.gazebo_process != None:
+            sig = signal.SIGTERM
+            os.killpg(os.getpgid(self.gazebo_process.pid), sig)
+            for i in range(0, len(self.script_process)):
+                os.killpg(os.getpgid(self.mavsdk_process[i].pid), sig)
+                os.killpg(os.getpgid(self.script_process[i].pid), sig)
+        root.destroy()
 
 
-def on_closing():
-    if gazebo != None:
-        sig = signal.SIGTERM
-        os.killpg(os.getpgid(gazebo.pid), sig)
-        for i in range(0, len(script)):
-            os.killpg(os.getpgid(mavsdk_server[i].pid), sig)
-            # os.kill(mavsdk_server[i].pid, sig)
-            os.killpg(os.getpgid(script[i].pid), sig)
-            # os.kill(script[i].pid, sig)
-    root.destroy()
-
-
-root = Tk()
-check_var = IntVar(value=1)
-# This is the section of code which creates the main window
-root.rowconfigure(tuple(range(2)), minsize=100)
-root.grid_columnconfigure(tuple(range(2)), uniform="button", minsize=220)
-
-sitl_frame = Frame(root)
-
-sitl_frame.columnconfigure(tuple(range(3)), weight=1)
-sitl_frame.grid(row=3, column=0, columnspan=2, sticky="ew")
-
-launch_btn = Button(
-    sitl_frame,
-    text="Launch SITL Simulation",
-    bg="#008B45",
-    font=("arial", 12, "normal"),
-    command=LaunchClickFunction,
-)
-launch_btn.grid(row=5, column=0, columnspan=3, sticky="e")
-
-select_firmware_btn = Button(
-    sitl_frame,
-    text="Select",
-    # bg="#008B45",
-    font=("arial", 12, "normal"),
-    command=SelectClickFunction,
-)
-select_firmware_btn.grid(row=4, column=2, sticky="e")
-
-Button(
-    root,
-    text="Start",
-    bg="#008B45",
-    font=("arial", 12, "normal"),
-    command=StartClickFunction,
-).grid(
-    row=1,
-    column=0,
-    sticky="nesw",
-)
-
-Button(
-    root,
-    text="Hold",
-    bg="#CD4F39",
-    font=("arial", 12, "normal"),
-    command=StopClickFunction,
-).grid(row=0, column=1, sticky="nesw")
-
-Button(
-    root,
-    text="Take Off",
-    bg="#CD4F39",
-    font=("arial", 12, "normal"),
-    command=TakeOffClickFunction,
-).grid(row=0, column=0, sticky="nesw")
-
-Button(
-    root,
-    text="Land",
-    bg="#CD4F39",
-    font=("arial", 12, "normal"),
-    command=LandClickFunction,
-).grid(row=1, column=1, sticky="nesw")
-
-sitl_check = Checkbutton(
-    root,
-    text="SITL",
-    variable=check_var,
-    onvalue=1,
-    offvalue=0,
-    command=SITLCheckFunction,
-)
-sitl_check.grid(row=2, column=0, columnspan=2, sticky="w")
-
-no_drones_label = Label(sitl_frame, text="Number of Drones:")
-no_drones_label.grid(row=3, column=0, sticky="w")
-
-firmware_label = Label(sitl_frame, text="PX4 Firmware Path:")
-firmware_label.grid(row=4, column=0, sticky="w")
-
-path_label = Label(sitl_frame)
-with open("config.txt") as f:
-    firmware_path = f.read()
-path_label.config(text=firmware_path)
-path_label.grid(row=4, column=1, sticky="w")
-
-drone_no_entry = Entry(sitl_frame)
-drone_no_entry.insert(END, "3")
-drone_no_entry.grid(row=3, column=1, sticky="w")
-
-root.title("Cascade Demo")
-root.iconphoto(True, tk.PhotoImage(file="../img/cascade-logo.png"))
-root.protocol("WM_DELETE_WINDOW", on_closing)
-root.resizable(False, False)
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
