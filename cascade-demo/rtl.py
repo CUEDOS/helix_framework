f939f8e62565aedc8267b918c7ec30f5f0c7caf1
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-25 05:37:07 -0500

    Inputs modified

--- cascade-demo/rtl.py
+++ cascade-demo/rtl.py
@@ -1,5 +1,5 @@
 def return_to_home(
-    drone, home_lat, home_long, rtl_altitude, rtl_start_lat, rtl_start_lon
+    drone, home_lat, home_long, rtl_altitude, rtl_start_lat, rrtl_start_lon
 ):
-
+    goto_location(rtl_start_lat, lrrtl_start_lon, rtl_altitude, 0)
     return
