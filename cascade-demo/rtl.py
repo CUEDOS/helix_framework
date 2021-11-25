0c694b28ed5f8b964a0da65620b228798bd44912
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-25 05:43:05 -0500

    Commands added

--- cascade-demo/rtl.py
+++ cascade-demo/rtl.py
@@ -1,5 +1,6 @@
 def return_to_home(
     drone, home_lat, home_long, rtl_altitude, rtl_start_lat, rrtl_start_lon
 ):
-    await goto_location(rtl_start_lat, lrrtl_start_lon, rtl_altitude, 0)
+    await drone.action.goto_location(rtl_start_lat, lrrtl_start_lon, rtl_altitude, 0)
+    await drone.action.goto_location(home_lat, lrrtl_start_lon, rtl_altitude, 0)
     return
