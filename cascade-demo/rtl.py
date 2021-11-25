a9dd9fcc02eb547ed21194788fd08c8663a6b8c9
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-25 05:47:57 -0500

    Ation Functions added

--- cascade-demo/rtl.py
+++ cascade-demo/rtl.py
@@ -2,5 +2,8 @@ def return_to_home(
     drone, home_lat, home_long, rtl_altitude, rtl_start_lat, rtl_start_lon
 ):
     await drone.action.goto_location(rtl_start_lat, rtl_start_lon, rtl_altitude, 0)
-    await drone.action.goto_location(home_lat, lome_long, rtl_altitude, 0)
+    if the dorne is at the right return altitude:
+        await drone.action.goto_location(home_lat, lome_long, rtl_altitude, 0)
+        if the drone is above the launch point:
+            await drone.action.land()    
     return
