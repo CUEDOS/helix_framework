712dfd89bc0f7d4e5253445063d3ff14e01e8391
Verdon Crann <verdon.crann@manchester.ac.uk>
2021-11-25 12:41:33 +0000

    fixed multi line comment

--- cascade-demo/gtools.py
+++ cascade-demo/gtools.py
@@ -1,32 +1,31 @@
 def alt_calc(alt_dict):
-"""
-Parameters
-------------
-alt_dict: Dict(key:drone_index (string), value: altitude (float))
+    """
+    Parameters
+    ------------
+    alt_dict: Dict(key:drone_index (string), value: altitude (float))
 
-Example:
-alt_dict = 
-{
- "P101": 20,
- "P102": 12,
- ...
- }
- x = alt_dict["P101"]
- print(x)
- # 20
+    Example:
+    alt_dict = 
+    {
+    "P101": 20,
+    "P102": 12,
+    ...
+    }
+    x = alt_dict["P101"]
+    print(x)
+    # 20
 
- Returns:
- --------
- output_dict: Dict(key:drone_index (string), value: altitude (float))
+    Returns:
+    --------
+    output_dict: Dict(key:drone_index (string), value: altitude (float))
+    """
+    
+    min_alt = 10 # meters - min return altitude above launch altitude
 
+    # TODO find a way to sort the altitudes, and therefore sorting the drones
 
-"""
-min_alt = 10 # meters - min return altitude above launch altitude
+    # TODO Make new dictionary with return altitudes
 
-# TODO find a way to sort the altitudes, and therefore sorting the drones
+    # TODO Ensure all altitudes are higher than min altitude
 
-# TODO Make new dictionary with return altitudes
-
-# TODO Ensure all altitudes are higher than min altitude
-
-return output_dict
+    return output_dict
