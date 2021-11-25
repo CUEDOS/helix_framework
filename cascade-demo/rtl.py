a8160b8a06489439ce1e7fa00def7490c3cb9d76
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-25 06:04:10 -0500

    Conditions Modified

--- cascade-demo/rtl.py
+++ cascade-demo/rtl.py
@@ -2,7 +2,7 @@ def return_to_home(
     drone, home_lat, home_long, rtl_altitude, rtl_start_lat, rtl_start_lon
 ):
     await drone.action.goto_location(rtl_start_lat, rtl_start_lon, rtl_altitude, 0)
-    if the dorne is at the right return altitude:
+    if abs(current_lat-home_lat)<0.0001 and abs(current_lon-home_long)<0.0001 and abs(current_alt-rtl_altitude)<0.05:
         await drone.action.goto_location(home_lat, home_long, rtl_altitude, 0)
         if the drone is above the launch point:
             await drone.action.land()    
