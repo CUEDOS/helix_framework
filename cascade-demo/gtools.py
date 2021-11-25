ae77c5d40f11275ec3fe273e44e5309b168185f8
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-25 14:27:01 -0500

    Creting return alts

--- cascade-demo/gtools.py
+++ cascade-demo/gtools.py
@@ -22,17 +22,15 @@ def alt_calc(alt_dict):
 
     min_alt = 10  # meters - min return altitude above launch altitude
 
-    for i in range(0,CONST_SWARM_SIZE-1) # Saving current altitudes in alts array
-        alts[i] = alt_dict["P" + str(i+101)]
+    for i in range(0, CONST_SWARM_SIZE - 1):  # Saving current altitudes in alts array
+        alts[i] = alt_dict["P" + str(i + 101)]
 
     alts.sort()  # Sorting alts
 
     return_alts_sorted
-    for i 
-    # TODO find a way to sort the altitudes, and therefore sorting the drones
+    for i in range(
+        0, CONST_SWARM_SIZE - 1
+    ):  # Now i shows the order of the drones in alts
+        alt_return_dict[alt_dict[alts[i]]] = return_alts_sorted[i]
 
-    # TODO Make new dictionary with return altitudes
-
-    # TODO Ensure all altitudes are higher than min altitude
-
-    return output_dict
+    return return_alts_sorted
