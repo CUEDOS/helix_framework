5730de44357944130b8088466f1ed1a3a6478a1e
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-26 01:42:50 -0500

    Creating & assignig return alts

--- cascade-demo/gtools.py
+++ cascade-demo/gtools.py
@@ -1,25 +1,36 @@
+from math import floor
 def alt_calc(alt_dict):
 
-    min_alt = 10  # meters - min return altitude above launch altitude
+    min_alt = 10  # in meters - min return altitude above launch altitude
+    alt_step = 2  # in meters - the alt difference between return alts
 
-    for i in range(0, CONST_SWARM_SIZE - 1):  # Saving current altitudes in alts array
+    for i in range(0, CONST_SWARM_SIZE - 1):  # Saving current altitudes in alts array ----------------------------------
         alts[i] = alt_dict["P" + str(i + 101)]
 
     alts.sort()    # Sorting alts
     
-    mean=0
-    for i in range(0, CONST_SWARM_SIZE - 1): # Calculating the mean of current alts
+    mean=0          # Calculating the mean of current alts --------------------------------------------------------------
+    for i in range(0, CONST_SWARM_SIZE - 1): 
         mean=mean+alts[i]
     mean=mean/CONST_SWARM_SIZE
     
-    alt_return_sorted
+    #Creating sorted return alts ------------------------------------------------------------------------------------------
+    middle_element=floor(CONST_SWARM_SIZE/2)
 
-    if alt_return_sorted[0]<min_alt:  # Checking minimum alt
+    if CONST_SWARM_SIZE%2 ==0:
+        alt_return_sorted[middle_element]=mean
+        for i in range (1, (CONST_SWARM_SIZE - 1)/2):
+            alt_return_sorted[middle_element-i]=mean-i*alt_step
+            alt_return_sorted[middle_element+i]=mean+i*alt_step
+    else:
+
+
+    if alt_return_sorted[0]<min_alt:  # Checking minimum alt ----------------------------------------------------------------
         difference=min_alt-alt_return_sorted[0]
         for i in range(0, CONST_SWARM_SIZE - 1):
             alt_return_sorted[i]=alt_return_sorted[i]+difference
 
-    for i in range(0, CONST_SWARM_SIZE - 1):  # assigning sorted alts (Now i shows the order of the drones in alts)
+    for i in range(0, CONST_SWARM_SIZE - 1):  # assigning sorted alts (Now i shows the order of the drones in alts) ----------
         alt_return_dict[alt_dict[alts[i]]] = alt_return_sorted[i]
 
     return alt_return_dict
