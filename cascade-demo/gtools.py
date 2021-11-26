d1d90622dfacd238b5bba22453e0389769bf974d
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-26 03:34:10 -0500

    Calcularing & assigning return alts

--- cascade-demo/gtools.py
+++ cascade-demo/gtools.py
@@ -3,12 +3,14 @@ def alt_calc(alt_dict):
 
     min_alt = 10  # in meters - min return altitude above launch altitude
     alt_step = 2  # in meters - the alt difference between return alts
+
     # Saving current altitudes in alts array -----------------------------------------------------------------------------
     alts=[0]*CONST_SWARM_SIZE               
     for i in range(0, CONST_SWARM_SIZE ):
         alts[i] = alt_dict["P" + str(i + 101)]
 
     alts.sort()    # Sorting alts
+    
     # Calculating the mean of current alts -------------------------------------------------------------------------------
     mean=0          
     for i in range(0, CONST_SWARM_SIZE ): 
@@ -29,11 +31,13 @@ def alt_calc(alt_dict):
             alt_return_sorted[middle_element-i]=mean-i*alt_step
             alt_return_sorted[middle_element+i]=mean+i*alt_step
         alt_return_sorted[0]=alt_return_sorted[1]-alt_step
+    
     # Checking minimum alt -----------------------------------------------------------------------------------------------
     if alt_return_sorted[0]<min_alt:  
         difference=min_alt-alt_return_sorted[0]
         for i in range(0, CONST_SWARM_SIZE):
             alt_return_sorted[i]=alt_return_sorted[i]+difference
+    
     # assigning sorted alts (Now i shows the order of the drones in alts) -----------------------------------------------
     for i in range(0, CONST_SWARM_SIZE):  
         alt_return_dict[alt_dict[alts[i]]] = alt_return_sorted[i]
