92184b700ceb047d35aa2de74585f18af73e41df
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-30 02:41:03 -0500

    Unit testing

--- cascade-demo/test_gtools.py
+++ cascade-demo/test_gtools.py
@@ -2,3 +2,5 @@ import unittest
 import gtools # importing the module we want to test (the test file should be in the same directory as module file)
 
 class TestGtools(): # inherit from unittest.TestCase 
+	def test_alt_calc (self):  # this is a method so it should start with test_ and then the function we want to test
+		alt_return_dict = gtools.alt_calc()  # file name. function name
