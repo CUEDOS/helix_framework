f74f64614a56c428deab8e51d4aa21012cefd89d
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-11-30 03:03:44 -0500

    Run it to test gtools

--- cascade-demo/test_gtools.py
+++ cascade-demo/test_gtools.py
@@ -1,8 +1,11 @@
 import unittest
 import gtools # importing the module we want to test (the test file should be in the same directory as module file)
 
-class TestGtools(): # inherit from unittest.TestCase 
+class TestGtools(unittest.TestCase): # inherit from unittest.TestCase 
 	def test_alt_calc (self):  # this is a method so it should start with test_ and then the function we want to test
 		alt_dict={"P101":25, "P102":12.5, "P103":11, "P104":21, "P105":18, "P106":14}
 		alt_return_dict = gtools.alt_calc(alt_dict)  # file name. function name
-		print (alt_return_dict)
+		print ("alt_return_dict=", alt_return_dict)
+
+if __name__ == '__main__':
+	unittest.main()
