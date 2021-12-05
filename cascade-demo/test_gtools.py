2b0e2205f93b68ca57949c67d658a96a53ec9bce
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-12-05 02:22:27 -0500

    asseting

--- cascade-demo/test_gtools.py
+++ cascade-demo/test_gtools.py
@@ -3,9 +3,11 @@ import gtools # importing the module we want to test its function (the test file
 
 class TestGtools(unittest.TestCase): # inherit from unittest.TestCase 
 	def test_alt_calc (self):  # this is a method so it should start with test_ and then the function we want to test
-		alt_dict={"P101":25, "P102":12.5, "P103":11, "P104":21, "P105":18, "P106":14}
+		alt_dict={"P101":25, "P102":12.5, "P103":11, "P104":21, "P105":18, "P106":14} # altitude set 1
+		alt_return_dict_1={"P101":12, "P102":10, "P103":16, "P104":14, "P105":20, "P106":18 } # correct dictionary for input set 1
 		alt_return_dict = gtools.alt_calc(alt_dict)  # module name. function name (input)
 		print ("alt_return_dict=", alt_return_dict)
+		self.assertEqual(alt_return_dict_1,alt_return_dict)
 
 if __name__ == '__main__':  # to be able to run the code directly
 	unittest.main()
