23758dde68d6ba36b12a55431e37e0fb7ce87412
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-12-08 06:27:22 -0500

    7 successful tests

--- cascade-demo/test_gtools.py
+++ cascade-demo/test_gtools.py
@@ -44,7 +44,7 @@ class TestGtools(unittest.TestCase): # inherit from unittest.TestCase
 		# print ("Set 5:")
 		# altitude set 5 (8 drones, all same altitudes)
 		alt_dict={"P101":20, "P102":20, "P103":20, "P104":20, "P105":20, "P106":20, "P107":20, "P108":20}
-		alt_return_dict_5={"P101":16.5, "P102":17.5, "P103":18.5, "P104":19.5, "P105":20.5, "P106":21.5, "P107":22.5, "P108":23.5} # output dictionary for input set 4
+		alt_return_dict_5={"P101":16.5, "P102":17.5, "P103":18.5, "P104":19.5, "P105":20.5, "P106":21.5, "P107":22.5, "P108":23.5} # output dictionary for input set 5
 		alt_return_dict = gtools.alt_calc(alt_dict)  # module name. function name (input)
 		# print ("alt_return_dict for set 5=", alt_return_dict)
 		self.assertEqual(alt_return_dict_5,alt_return_dict) # assessing outputs of set 5
@@ -52,12 +52,21 @@ class TestGtools(unittest.TestCase): # inherit from unittest.TestCase
 	def test_alt_calc_6 (self):  # this is a method so it should start with test_ and then the function we want to test
 	# input output set 6 -----------------------------------------------
 		# print ("Set 6:")
-		# altitude set 6 (7 drones, three same altitudes, huge differences)
+		# altitude set 6 (7 drones, three & two same altitudes, huge differences)
 		alt_dict={"P101":1000, "P102":0, "P103":1, "P104":1, "P105":1, "P106":20000, "P107":0}
-		alt_return_dict_5={"P101":16.5, "P102":17.5, "P103":18.5, "P104":19.5, "P105":20.5, "P106":21.5, "P107":22.5} # output dictionary for input set 4
+		alt_return_dict_6={"P101":99, "P102":94, "P103":96, "P104":97, "P105":98, "P106":100, "P107":95} # output dictionary for input set 6
 		alt_return_dict = gtools.alt_calc(alt_dict)  # module name. function name (input)
-		# print ("alt_return_dict for set 5=", alt_return_dict)
-		self.assertEqual(alt_return_dict_5,alt_return_dict) # assessing outputs of set 5
+		# print ("alt_return_dict for set 6=", alt_return_dict)
+		self.assertEqual(alt_return_dict_6,alt_return_dict) # assessing outputs of set 6
+	def test_alt_calc_7 (self):  # this is a method so it should start with test_ and then the function we want to test
+	# input output set 7 -----------------------------------------------
+		# print ("Set 7:")
+		# altitude set 7 (10 drones, four & two same altitudes)
+		alt_dict={"P101":3, "P102":2, "P103":4, "P104":3, "P105":3, "P106":3, "P107":9.5, "P108":3, "P109":11, "P110":2}
+		alt_return_dict_7={"P101":12, "P102":10, "P103":17, "P104":13, "P105":14, "P106":15, "P107":18, "P108":16, "P109":19, "P110":11} # output dictionary for input set 7
+		alt_return_dict = gtools.alt_calc(alt_dict)  # module name. function name (input)
+		# print ("alt_return_dict for set 7=", alt_return_dict)
+		self.assertEqual(alt_return_dict_7,alt_return_dict) # assessing outputs of set 7
 
 if __name__ == '__main__':  # to be able to run the code directly
 	unittest.main()
