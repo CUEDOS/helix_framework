c7edd3a31ae9aabd95611c22b751c31eeab2db5d
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-12-08 05:41:45 -0500

    Ready to test

--- cascade-demo/test_gtools.py
+++ cascade-demo/test_gtools.py
@@ -42,9 +42,19 @@ class TestGtools(unittest.TestCase): # inherit from unittest.TestCase
 	def test_alt_calc_5 (self):  # this is a method so it should start with test_ and then the function we want to test
 	# input output set 5 -----------------------------------------------
 		# print ("Set 5:")
-		# altitude set 5 (even number of drones, all same altitudes)
+		# altitude set 5 (8 drones, all same altitudes)
 		alt_dict={"P101":20, "P102":20, "P103":20, "P104":20, "P105":20, "P106":20, "P107":20, "P108":20}
 		alt_return_dict_5={"P101":16.5, "P102":17.5, "P103":18.5, "P104":19.5, "P105":20.5, "P106":21.5, "P107":22.5, "P108":23.5} # output dictionary for input set 4
+		alt_return_dict = gtools.alt_calc(alt_dict)  # module name. function name (input)
+		# print ("alt_return_dict for set 5=", alt_return_dict)
+		self.assertEqual(alt_return_dict_5,alt_return_dict) # assessing outputs of set 5
+	
+	def test_alt_calc_6 (self):  # this is a method so it should start with test_ and then the function we want to test
+	# input output set 6 -----------------------------------------------
+		# print ("Set 6:")
+		# altitude set 6 (7 drones, three same altitudes, huge differences)
+		alt_dict={"P101":1000, "P102":0, "P103":1, "P104":1, "P105":1, "P106":20000, "P107":0}
+		alt_return_dict_5={"P101":16.5, "P102":17.5, "P103":18.5, "P104":19.5, "P105":20.5, "P106":21.5, "P107":22.5} # output dictionary for input set 4
 		alt_return_dict = gtools.alt_calc(alt_dict)  # module name. function name (input)
 		# print ("alt_return_dict for set 5=", alt_return_dict)
 		self.assertEqual(alt_return_dict_5,alt_return_dict) # assessing outputs of set 5
