bc4170ef7548ef43b7a4f90a41470657472e4c68
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-12-14 14:06:13 -0500

    Update test_gtools.py

--- tests/test_gtools.py
+++ tests/test_gtools.py
@@ -4,26 +4,50 @@ import numpy as np
 from helixio.communication import AgentTelemetry
 from math import sqrt
 
+# test of alt_dict function ----------------------------------------------------------------------------------------------------------------------------------
 site_elevation=480
 
-# Set1: even number of drones----------------------------------------------
+# Set1: even number of drones-------------------------------------------------
 alt_dict_1={"P101":12, "P102":10, "P103":16, "P104":14, "P105":20, "P106":18}
 alt_return_dict_1={"P101":13.5, "P102":12.5, "P103":15.5, "P104":14.5, "P105":17.5, "P106":16.5}
 for key in alt_return_dict_1.keys():
     alt_return_dict_1[key]+=site_elevation
  
-#Set2: even number of drones, two same altitudes--------------------------
+#Set2: even number of drones, two same altitudes------------------------------
 alt_dict_2={"P101":12, "P102":10, "P103":15, "P104":15, "P105":20, "P106":18}
 alt_return_dict_2={"P101":13.5, "P102":12.5, "P103":14.5, "P104":15.5, "P105":17.5, "P106":16.5}
 for key in alt_return_dict_2.keys():
     alt_return_dict_2[key]+=site_elevation
  
-#Set3: odd number of drones------------------------------------------------
+#Set3: odd number of drones----------------------------------------------------
 alt_dict_3={"P101":3, "P102":2, "P103":4, "P104":3, "P105":3, "P106":3, "P107":9.5, "P108":3, "P109":11, "P110":2}
 alt_return_dict_3={"P101":12, "P102":10, "P103":17, "P104":13, "P105":14, "P106":15, "P107":18, "P108":16, "P109":19, "P110":11}
 for key in alt_return_dict_3.keys():
     alt_return_dict_3[key]+=site_elevation
-
+    
+#Set4: odd number of drones, two same altitudes--------------------------------
+alt_dict_4={"P101":20, "P102":15, "P103":21, "P104":20, "P105":14}
+alt_return_dict_4={"P101":18, "P102":17, "P103":20, "P104":19, "P105":16}
+for key in alt_return_dict_4.keys():
+    alt_return_dict_4[key]+=site_elevation
+    
+#Set5: 8 drones, all same altitudes--------------------------------------------
+alt_dict_5={"P101":20, "P102":20, "P103":20, "P104":20, "P105":20, "P106":20, "P107":20, "P108":20}
+alt_return_dict_5={"P101":16.5, "P102":17.5, "P103":18.5, "P104":19.5, "P105":20.5, "P106":21.5, "P107":22.5, "P108":23.5}
+for key in alt_return_dict_5.keys():
+    alt_return_dict_5[key]+=site_elevation
+    
+# Set6: 7 drones, three & two same altitudes, huge differences -----------------
+alt_dict_6={"P101":1000, "P102":0, "P103":1, "P104":1, "P105":1, "P106":20000, "P107":0}
+alt_return_dict_6={"P101":99, "P102":94, "P103":96, "P104":97, "P105":98, "P106":100, "P107":95}
+for key in alt_return_dict_6.keys():
+    alt_return_dict_6[key]+=site_elevation
+ 
+#Set7:# 10 drones, four & two same altitudes  ----------------------------------
+alt_dict_7={"P101":3, "P102":2, "P103":4, "P104":3, "P105":3, "P106":3, "P107":9.5, "P108":3, "P109":11, "P110":2}
+alt_return_dict_7={"P101":12, "P102":10, "P103":17, "P104":13, "P105":14, "P106":15, "P107":18, "P108":16, "P109":19, "P110":11}
+for key in alt_return_dict_7.keys():
+    alt_return_dict_7[key]+=site_elevation
 
 @pytest.mark.parametrize(
     "dict_in, dict_out",
@@ -38,8 +62,8 @@ for key in alt_return_dict_3.keys():
 def test_alt_calc(dict_in, dict_out):
     assert alt_calc(dict_in,site_elevation) == dict_out
 
-
-# -------------Set1
+#test of proximity_check function -------------------------------------------------------------------------------------------
+# Set1: ------------------------
 
 swarm_telemetry_1 = {
     "P101": None,
@@ -63,7 +87,7 @@ output_1 = [
     ["P103", "P104", 0],
 ]
 
-# --------------Set2
+# Set2: ---------------------------
 swarm_telemetry_2 = {
     "P101": None,
     "P102": None,
