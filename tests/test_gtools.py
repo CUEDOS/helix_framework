2d5472a14544f463b4780ba31914f05ea9f8b286
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-12-15 09:59:23 -0500

    Update test_gtools.py

--- tests/test_gtools.py
+++ tests/test_gtools.py
@@ -5,12 +5,7 @@ from helixio.communication import AgentTelemetry
 from math import sqrt
 
 # test of alt_dict function ----------------------------------------------------------------------------------------------------------------------------------
-    
-#Set5: 8 drones, all same altitudes--------------------------------------------
-alt_dict_5={"P101":20, "P102":20, "P103":20, "P104":20, "P105":20, "P106":20, "P107":20, "P108":20}
-alt_return_dict_5={"P101":16.5, "P102":17.5, "P103":18.5, "P104":19.5, "P105":20.5, "P106":21.5, "P107":22.5, "P108":23.5}
-
-    
+   
 # Set6: 7 drones, three & two same altitudes, huge differences -----------------
 alt_dict_6={"P101":1000, "P102":0, "P103":1, "P104":1, "P105":1, "P106":20000, "P107":0}
 alt_return_dict_6={"P101":99, "P102":94, "P103":96, "P104":97, "P105":98, "P106":100, "P107":95}
@@ -23,23 +18,31 @@ alt_return_dict_7={"P101":12, "P102":10, "P103":17, "P104":13, "P105":14, "P106"
 @pytest.mark.parametrize(
     "dict_in, site_elevation, dict_out",
     [({"P101": 22, "P102": 20, "P103": 26, "P104": 24, "P105": 30, "P106": 28}, # input Set1: even number of drones
-      10, 
+      10, # site_elevation Set1
       {"P101": 23.5, "P102": 22.5, "P103": 25.5, "P104": 24.5, "P105": 27.5, "P106": 26.5}), # output Set1
      
      ({"P101": 32, "P102": 30, "P103": 35, "P104": 35, "P105": 40, "P106": 38}, # input Set2: even number of drones, two same altitudes 
-      20, 
+      20, # site_elevation Set2
       {"P101": 33.5, "P102": 32.5, "P103": 34.5, "P104": 35.5, "P105": 37.5, "P106": 36.5}), # output Set2
      
      ({"P101": 483, "P102": 482, "P103": 484, "P104": 483, "P105": 483, "P106": 483, "P107": 489.5, "P108": 483, "P109": 491, "P110": 482}, # Set3: 10 drones, mean below min+site_elevation
-      480, 
-      "P101": 492, "P102": 490, "P103": 497, "P104": 493, "P105": 494, "P106": 495, "P107": 498, "P108": 496, "P109": 499, "P110": 491}), # output Set3
+      480, # site_elevation Set3
+      {"P101": 492, "P102": 490, "P103": 497, "P104": 493, "P105": 494, "P106": 495, "P107": 498, "P108": 496, "P109": 499, "P110": 491}), # output Set3
      
      ({"P101": 10, "P102": 5, "P103": 11, "P104": 10, "P105": 4}, # input Set4: odd number of drones, two same altitudes
-      -10,
-      {"P101": 8, "P102": 7, "P103": 10, "P104": 9, "P105": 6}),
-     (alt_dict_5, -20, alt_return_dict_5),
-     (alt_dict_6, 30, alt_return_dict_6),
-     (alt_dict_7, 1000, alt_return_dict_7)]
+      -10, # site_elevation Set4
+      {"P101": 8, "P102": 7, "P103": 10, "P104": 9, "P105": 6}), # output Set4
+     
+     ({"P101": 0, "P102": 0, "P103": 0, "P104": 0, "P105": 0, "P106": 0, "P107": 0, "P108": 0}, # input Set5: 8 drones, all same altitudes
+      -20, 
+      {"P101": -3.5, "P102": -2.5, "P103": -1.5, "P104": -0.5, "P105": 0.5, "P106": 1.5, "P107": 2.5, "P108": 3.5}), # output Set5
+     
+     (alt_dict_6, 
+      30, 
+      alt_return_dict_6),
+     (alt_dict_7, 
+      1000, 
+      alt_return_dict_7)]
 )
 
 def test_alt_calc(dict_in, site_elevation, dict_out):
