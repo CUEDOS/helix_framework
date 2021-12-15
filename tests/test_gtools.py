9c94ea8fd9d03d1f23e6f9acd3196287e086cf69
Peyman Amiri <93652974+PeymanAmirii@users.noreply.github.com>
2021-12-15 09:49:06 -0500

    Update test_gtools.py

--- tests/test_gtools.py
+++ tests/test_gtools.py
@@ -5,25 +5,6 @@ from helixio.communication import AgentTelemetry
 from math import sqrt
 
 # test of alt_dict function ----------------------------------------------------------------------------------------------------------------------------------
-
-# Set1: even number of drones-------------------------------------------------
-alt_dict_1={"P101":12, "P102":10, "P103":16, "P104":14, "P105":20, "P106":18}
-alt_return_dict_1={"P101":13.5, "P102":12.5, "P103":15.5, "P104":14.5, "P105":17.5, "P106":16.5}
-
- 
-#Set2: even number of drones, two same altitudes------------------------------
-alt_dict_2={"P101":12, "P102":10, "P103":15, "P104":15, "P105":20, "P106":18}
-alt_return_dict_2={"P101":13.5, "P102":12.5, "P103":14.5, "P104":15.5, "P105":17.5, "P106":16.5}
- 
-#Set3: odd number of drones----------------------------------------------------
-alt_dict_3={"P101":3, "P102":2, "P103":4, "P104":3, "P105":3, "P106":3, "P107":9.5, "P108":3, "P109":11, "P110":2}
-alt_return_dict_3={"P101":12, "P102":10, "P103":17, "P104":13, "P105":14, "P106":15, "P107":18, "P108":16, "P109":19, "P110":11}
-
-    
-#Set4: odd number of drones, two same altitudes--------------------------------
-alt_dict_4={"P101":20, "P102":15, "P103":21, "P104":20, "P105":14}
-alt_return_dict_4={"P101":18, "P102":17, "P103":20, "P104":19, "P105":16}
-
     
 #Set5: 8 drones, all same altitudes--------------------------------------------
 alt_dict_5={"P101":20, "P102":20, "P103":20, "P104":20, "P105":20, "P106":20, "P107":20, "P108":20}
@@ -40,11 +21,22 @@ alt_dict_7={"P101":3, "P102":2, "P103":4, "P104":3, "P105":3, "P106":3, "P107":9
 alt_return_dict_7={"P101":12, "P102":10, "P103":17, "P104":13, "P105":14, "P106":15, "P107":18, "P108":16, "P109":19, "P110":11}
 
 @pytest.mark.parametrize(
-    "dict_in, dict_out",
-    [(alt_dict_1, 10, alt_return_dict_1),
-     (alt_dict_2, 20, alt_return_dict_2),
-     (alt_dict_3, 480, alt_return_dict_3),
-     (alt_dict_4, -10, alt_return_dict_4),
+    "dict_in, site_elevation, dict_out",
+    [({"P101": 22, "P102": 20, "P103": 26, "P104": 24, "P105": 30, "P106": 28}, # input Set1: even number of drones
+      10, 
+      {"P101": 23.5, "P102": 22.5, "P103": 25.5, "P104": 24.5, "P105": 27.5, "P106": 26.5}), # output Set1
+     
+     ({"P101": 32, "P102": 30, "P103": 35, "P104": 35, "P105": 40, "P106": 38}, # input Set2: even number of drones, two same altitudes 
+      20, 
+      {"P101": 33.5, "P102": 32.5, "P103": 34.5, "P104": 35.5, "P105": 37.5, "P106": 36.5}), # output Set2
+     
+     ({"P101": 483, "P102": 482, "P103": 484, "P104": 483, "P105": 483, "P106": 483, "P107": 489.5, "P108": 483, "P109": 491, "P110": 482}, # Set3: 10 drones, mean below min+site_elevation
+      480, 
+      "P101": 492, "P102": 490, "P103": 497, "P104": 493, "P105": 494, "P106": 495, "P107": 498, "P108": 496, "P109": 499, "P110": 491}), # output Set3
+     
+     ({"P101": 10, "P102": 5, "P103": 11, "P104": 10, "P105": 4}, # input Set4: odd number of drones, two same altitudes
+      -10,
+      {"P101": 8, "P102": 7, "P103": 10, "P104": 9, "P105": 6}),
      (alt_dict_5, -20, alt_return_dict_5),
      (alt_dict_6, 30, alt_return_dict_6),
      (alt_dict_7, 1000, alt_return_dict_7)]
@@ -124,141 +116,6 @@ def test_proximity_check(swarm_telemetry, output):
     
     
     # New code lots of errors, need fixing
-    
-    from helixio.gtools import (
-    alt_calc,
-    proximity_check,
-)  # importing the module we want to test its function (the test file should be in the same directory as module file)
-import pytest
-import numpy as np
-from helixio.communication import AgentTelemetry
-from math import sqrt
-
-
-@pytest.mark.parametrize(
-    "dict_in, site_elevation, dict_out",
-    [
-        (
-            {"P101": 12, "P102": 10},
-            10,
-            {
-                "P101": 13.5,
-                "P102": 12.5
-            },
-        ),  # Set1: even number of drones
-        (
-            {"P101": 12, "P102": 10, "P103": 15, "P104": 15, "P105": 20, "P106": 18},
-            {
-                "P101": 13.5,
-                "P102": 12.5,
-                "P103": 14.5,
-                "P104": 15.5,
-                "P105": 17.5,
-                "P106": 16.5,
-            }
-        ),  # Set2: even number of drones, two same altitudes
-        (
-            {
-                "P101": 3,
-                "P102": 2,
-                "P103": 4,
-                "P104": 3,
-                "P105": 3,
-                "P106": 3,
-                "P107": 9.5,
-                "P108": 3,
-                "P109": 11,
-                "P110": 2,
-            },
-            {
-                "P101": 12,
-                "P102": 10,
-                "P103": 17,
-                "P104": 13,
-                "P105": 14,
-                "P106": 15,
-                "P107": 18,
-                "P108": 16,
-                "P109": 19,
-                "P110": 11,
-            }
-        ),  # Set3: odd number of drones
-        (
-            {"P101": 20, "P102": 15, "P103": 21, "P104": 20, "P105": 14},
-            {"P101": 18, "P102": 17, "P103": 20, "P104": 19, "P105": 16},
-        ),  # Set4: odd number of drones, two same altitudes
-        (
-            {
-                "P101": 20,
-                "P102": 20,
-                "P103": 20,
-                "P104": 20,
-                "P105": 20,
-                "P106": 20,
-                "P107": 20,
-                "P108": 20,
-            },
-            {
-                "P101": 16.5,
-                "P102": 17.5,
-                "P103": 18.5,
-                "P104": 19.5,
-                "P105": 20.5,
-                "P106": 21.5,
-                "P107": 22.5,
-                "P108": 23.5,
-            }
-        ),  # Set5:8 drones, all same altitudes
-        (
-            {
-                "P101": 1000,
-                "P102": 0,
-                "P103": 1,
-                "P104": 1,
-                "P105": 1,
-                "P106": 20000,
-                "P107": 0,
-            },
-            {
-                "P101": 99,
-                "P102": 94,
-                "P103": 96,
-                "P104": 97,
-                "P105": 98,
-                "P106": 100,
-                "P107": 95,
-            },
-        ),  # Set6: 7 drones, three & two same altitudes, huge differences
-        (
-            {
-                "P101": 3,
-                "P102": 2,
-                "P103": 4,
-                "P104": 3,
-                "P105": 3,
-                "P106": 3,
-                "P107": 9.5,
-                "P108": 3,
-                "P109": 11,
-                "P110": 2,
-            },
-            {
-                "P101": 12,
-                "P102": 10,
-                "P103": 17,
-                "P104": 13,
-                "P105": 14,
-                "P106": 15,
-                "P107": 18,
-                "P108": 16,
-                "P109": 19,
-                "P110": 11,
-            },
-        ),  # Set7: 10 drones, four & two same altitudes
-    ],
-)
-def test_alt_calc(dict_in, site_elevation, dict_out):
-    assert alt_calc(dict_in, site_elevation) == dict_out
 
 
 # -------------Set1
